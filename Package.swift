f6e4ff00d3907a307cd26ec557e101936275b5c1
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-18 19:18:31 -0400

    Created Swift package

---
+++ Package.swift
@@ -0,0 +1,62 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+    name: "Vgate",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "Vgate",
+            targets: ["Vgate"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/PureSwift/Bluetooth.git",
+            from: "6.0.0"
+        ),
+        .package(
+            url: "https://github.com/PureSwift/GATT.git",
+            from: "3.2.0"
+        )
+    ],
+    targets: [
+        .target(
+            name: "Vgate",
+            dependencies: [
+                .product(
+                    name: "Bluetooth",
+                    package: "Bluetooth"
+                ),
+                .product(
+                    name: "GATT",
+                    package: "GATT"
+                )
+            ]
+        ),
+        .testTarget(
+            name: "VgateTests",
+            dependencies: [
+                "Vgate",
+                .product(
+                    name: "Bluetooth",
+                    package: "Bluetooth"
+                ),
+                .product(
+                    name: "BluetoothGAP",
+                    package: "Bluetooth",
+                    condition: .when(platforms: [.macOS, .linux])
+                ),
+                .product(
+                    name: "GATT",
+                    package: "GATT"
+                )
+            ]
+        )
+    ]
+)
---
+++ Sources/Vgate/Vgate.swift
@@ -0,0 +1,8 @@
+import Foundation
+import Bluetooth
+import GATT
+
+public enum VgateAccessory: Equatable, Hashable, Sendable {
+    
+    
+}
